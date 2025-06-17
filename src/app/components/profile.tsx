11ed55809ebe2de67f8dfac5275d122d7875eaf1
Austin Hunt <austinhunt818@u.boisestate.edu>
2025-06-16 19:55:28 -0600

    Flesh out profile section and add images of myself

--- src/app/components/profile.tsx
+++ src/app/components/profile.tsx
@@ -1,5 +1,7 @@
 'use client';
 
+import Image from 'next/image';
+
 export function Profile() {
     return (
         <div id='profile' className="centered-container flex flex-col gap-4 p-8 dark:bg-gray-100 bg-gray-900 dark:text-black text-white rounded-lg shadow-lg w-4/5 mx-auto">
@@ -9,6 +11,27 @@ export function Profile() {
             <p className="text-lg text-center sm:text-left">
                 I am a recent college graduate with a passion for software and web development. I am currently seeking a full-time position where I can apply my skills and continue to grow as a developer.
             </p>
+            <p className="text-lg text-center sm:text-left">
+                In addition to software development, I also enjoy music. I play many intstruments, but by far the most notable is saxophone. I have gotten to perform for thousands of people with the Blue Thunder Marching Band at Boise State.
+            </p>
+            <div className="flex justify-center">
+                <img
+                    src="/images/btmb.jpg"
+                    alt="Austin Hunt"
+                    width={350}
+                    className=" border-2 border-gray-500 dark:border-gray-800"
+                />
+                <img
+                    src="/images/sax.jpg"
+                    alt="Austin Hunt"
+                    width={550}
+                    className=" border-2 border-gray-500 dark:border-gray-800 ml-4"
+                />
+            </div>
+            <p className="text-lg text-center sm:text-left">
+                I love to try to combine my passions for software and music, as demonstrated by my Jingle Composer project.
+            </p>
+            
             
         </div>
     );
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -11,10 +11,19 @@ export default function Home() {
         <h1 className="text-6xl font-bold text-center">
           Hi, I&apos;m <br></br>Austin Hunt
         </h1>
+        <img
+            src="/images/me.jpeg"
+            alt="Austin Hunt"
+            width={250}
+            className="rounded-full border-2 border-gray-500 dark:border-gray-300 mx-auto"
+        />
         <Socials />
         <Profile />
         <br></br>
         <Projects />
+        <h2 className="text-2xl font-semibold text-center">
+          This website is still under development. Come back soon for details on more projects, and more information about me!
+        </h2>
 
         <NavDrawer />
       </main>
