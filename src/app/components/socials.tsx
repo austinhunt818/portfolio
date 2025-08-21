f1599db7bf592bbf38f48adb343d1fa926bf20b7
Austin Hunt <122411917+austinhunt818@users.noreply.github.com>
2025-08-21 12:53:31 -0600

    Fix resume button colors in dark mode

--- src/app/components/socials.tsx
+++ src/app/components/socials.tsx
@@ -32,14 +32,19 @@ export function Socials(){
 
     return(
         <div className="flex gap-10 justify-center items-center spacer-bottom ">
-            <a href='/AustinHuntResume.pdf' className={isDarkMode ? "transition-transform hover:scale-110 dark-resume-button" :"transition-transform hover:scale-110 resume-button"} target="_blank" rel="noopener noreferrer">
+            <a
+                href='/AustinHuntResume.pdf'
+                className="transition-transform hover:scale-110 resume-button"
+                target="_blank"
+                rel="noopener noreferrer"
+            >
                 Resume
             </a>
             <a href="https://github.com/austinhunt818" className="transition-transform hover:scale-125">
                 <img src={isDarkMode ? "logos/github-mark-white.svg" : "logos/github-mark.svg"} alt="GitHub" className="h-12 w-12" />
             </a>
             <a href="https://www.linkedin.com/in/austin-hunt-0183342b4/" className="transition-transform hover:scale-125">
-                <img src="logos/linkedin.svg" alt="LinkedIn" className="h-12 w-12" /> 
+                <img src="logos/linkedin.svg" alt="LinkedIn" className="h-12 w-12" />
             </a>
         </div>
     );
